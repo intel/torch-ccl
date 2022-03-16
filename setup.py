29f045d024708db4e0ef7f718cf1f7d0e76a5312
johnlu <chengjun.lu@intel.com>
2022-03-17 00:38:53 +0800

    Update the oneCCL to latest master

--- setup.py
+++ setup.py
@@ -98,6 +98,8 @@ class BuildCMakeExt(BuildExtension):
             # The value cannot be easily obtained in CMakeLists.txt.
             'CMAKE_PREFIX_PATH': torch.utils.cmake_prefix_path,
             'PYTORCH_LIBRARY_DIRS': CMakeExtension.convert_cmake_dirs(library_paths()),
+            # Enable the RPATH of the oneCCL
+            'ENABLE_LINKER_RPATH': 'ON',
             # skip the example and test code in oneCCL
             'BUILD_EXAMPLES': 'OFF',
             'BUILD_CONFIG': 'OFF',

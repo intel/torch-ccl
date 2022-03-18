a5822f32caa71f44c3c6b4f4cf723394da8ea3a3
johnlu <chengjun.lu@intel.com>
2022-03-18 18:43:25 +0800

    Change the ENABLE_LINKER_RPATH -> ENABLE_LINKER_RUNPATH.

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ class BuildCMakeExt(BuildExtension):
             'CMAKE_PREFIX_PATH': torch.utils.cmake_prefix_path,
             'PYTORCH_LIBRARY_DIRS': CMakeExtension.convert_cmake_dirs(library_paths()),
             # Enable the RPATH of the oneCCL
-            'ENABLE_LINKER_RPATH': 'ON',
+            'ENABLE_LINKER_RUNPATH': 'ON',
             # skip the example and test code in oneCCL
             'BUILD_EXAMPLES': 'OFF',
             'BUILD_CONFIG': 'OFF',

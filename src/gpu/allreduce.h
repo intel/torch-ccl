56f9c8baf1c91ec7aa825047b96ec5cd800d97d3
Cao Zhong Z <zhong.z.cao@intel.com>
2023-09-09 15:46:34 +0900

    Enable unix domain socket exchange
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- src/gpu/allreduce.h
+++ src/gpu/allreduce.h
@@ -1,7 +1,13 @@
 #include <sys/mman.h>
 #include <sys/syscall.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <poll.h>
 #include <system_error>
+#include <future>
 
 #include <mpi.h>
 #include <sycl/sycl.hpp>
@@ -46,6 +52,197 @@ struct exchange_contents
         std::make_error_code(std::errc(errno)));  \
   }
 
+// We can't inherit it from cmsghdr because flexible array member
+struct exchange_fd {
+  char obscure[CMSG_LEN(sizeof(int)) - sizeof(int)];
+  int fd;
+
+  exchange_fd(int cmsg_level, int cmsg_type, int fd)
+    : fd(fd) {
+    auto* cmsg = reinterpret_cast<cmsghdr *>(obscure);
+    cmsg->cmsg_len = sizeof(exchange_fd);
+    cmsg->cmsg_level = cmsg_level;
+    cmsg->cmsg_type = cmsg_type;
+  }
+
+  exchange_fd() : fd(-1) {
+    memset(obscure, 0, sizeof(obscure));
+  };
+};
+
+void un_send_fd(int sock, int fd, int rank, size_t offset) {
+  iovec iov[1];
+  msghdr msg;
+  auto rank_offset = std::make_pair(rank, offset);
+
+  iov[0].iov_base = &rank_offset;
+  iov[0].iov_len = sizeof(rank_offset);
+  msg.msg_iov = iov;
+  msg.msg_iovlen = 1;
+  msg.msg_name = nullptr;
+  msg.msg_namelen = 0;
+
+  exchange_fd cmsg (SOL_SOCKET, SCM_RIGHTS, fd);
+
+  msg.msg_control = &cmsg;
+  msg.msg_controllen = sizeof(exchange_fd);
+  sysCheck(sendmsg(sock, &msg, 0));
+}
+
+std::tuple<int, int, size_t> un_recv_fd(int sock) {
+  iovec iov[1];
+  msghdr msg;
+  std::pair<int, size_t> rank_offset;
+
+  iov[0].iov_base = &rank_offset;
+  iov[0].iov_len = sizeof(rank_offset);
+  msg.msg_iov = iov;
+  msg.msg_iovlen = 1;
+  msg.msg_name = nullptr;
+  msg.msg_namelen = 0;
+
+  exchange_fd cmsg;
+  msg.msg_control = &cmsg;
+  msg.msg_controllen = sizeof(exchange_fd);
+  int n_recv = recvmsg(sock, &msg, 0);
+  sysCheck(n_recv);
+  // assert(n_recv == sizeof(int));
+
+  return std::make_tuple(cmsg.fd, rank_offset.first, rank_offset.second);
+}
+
+int prepare_socket(const char *sockname) {
+  sockaddr_un un;
+  memset(&un, 0, sizeof(un));
+  un.sun_family = AF_UNIX;
+  strcpy(un.sun_path, sockname);
+
+  auto sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  sysCheck(sock);
+
+  int on = 1;
+  sysCheck(ioctl(sock, FIONBIO, &on));
+
+  auto size = offsetof(sockaddr_un, sun_path) + strlen(un.sun_path);
+  sysCheck(bind(sock, (sockaddr *)&un, size));
+
+  return sock;
+}
+
+int server_listen(const char *sockname) {
+  // unlink(sockname);
+  auto sock = prepare_socket(sockname);
+  sysCheck(listen(sock, 10));
+
+  return sock;
+}
+
+int serv_accept(int listen_sock) {
+  sockaddr_un  un;
+
+  socklen_t len = sizeof(un);
+  auto accept_sock = accept(listen_sock, (sockaddr *)&un, &len);
+  sysCheck(accept_sock);
+
+  return accept_sock;
+}
+
+int client_connect(const char *server, const char *client) {
+  auto sock = prepare_socket(client);
+  sockaddr_un sun;
+  memset(&sun, 0, sizeof(sun));
+  sun.sun_family = AF_UNIX;
+  strcpy(sun.sun_path, server);
+  auto len = offsetof(sockaddr_un, sun_path) + strlen(server);
+  sysCheck(connect(sock, (sockaddr *)&sun, len));
+  return sock;
+}
+
+void un_allgather(exchange_contents send_buf, exchange_contents recv_buf[], int rank, int world) {
+  const char* servername_prefix = "/tmp/open-peer-ipc-mem-server-rank_";
+  const char* clientname_prefix = "/tmp/open-peer-ipc-mem-client-rank_";
+  char server_name[64];
+  snprintf(server_name, sizeof(server_name), "%s%d", servername_prefix, rank);
+  unlink(server_name);
+  auto s_listen = server_listen(server_name);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  pollfd fdarray[world];
+  int recv_socks[world-1];
+
+  auto fd_guard = [&]() {
+    for (int i = 0, j = 0; i < world; ++ i) {
+      if ( i != rank )
+        sysCheck(close(recv_socks[j++]));
+      sysCheck(close(fdarray[i].fd));
+    }
+  };
+
+  struct guard__{
+    using F = decltype(fd_guard);
+    F f;
+    guard__(const F &f) : f(f) {}
+    ~guard__() { f(); }
+  } free_fd(fd_guard);
+
+  // connect to all ranks
+  for (int i = 0; i < world; ++ i) {
+    if (rank == i) {
+      fdarray[i].fd = s_listen;
+      fdarray[i].events = POLLIN;
+      fdarray[i].revents = 0;
+    } else {
+      char peer_name[64];
+      char client_name[64];
+
+      snprintf(client_name, sizeof(client_name), "%s%d-%d", clientname_prefix, rank, i);
+      unlink(client_name);
+
+      snprintf(peer_name, sizeof(peer_name), "%s%d", servername_prefix, i);
+      fdarray[i].fd = client_connect(peer_name, client_name);
+      fdarray[i].events = POLLOUT;
+      fdarray[i].revents = 0;
+    }
+  }
+
+  // std::future<std::tuple<int, int, size_t>> future_fds[world -1];
+  int slot = 0;
+  uint32_t send_progress = 1<<rank;
+
+  while (slot < world-1 || send_progress != (1<<world) -1) {
+    sysCheck(ppoll(fdarray, world, nullptr, nullptr));
+
+    for (int i = 0; i < world; ++ i) {
+      if (i == rank && (fdarray[i].revents & POLLIN)) {
+        // auto accept_sock = serv_accept(fdarray[i].fd);
+        // future_fds[slot ++] = std::async(
+        //     std::launch::async, [=]() {
+        //     struct sock_guard{
+        //       int sock;
+        //       sock_guard(int sock) : sock(sock) {}
+        //       ~guard_sock() {sysCheck(close(sock));}
+        //     } release(accept_sock);
+        //     auto ret = un_recv_fd(accept_sock);
+        //     return ret;});
+        recv_socks[slot ++] = serv_accept(fdarray[i].fd);
+      } else if ((send_progress & (1<<i)) == 0 && fdarray[i].revents & POLLOUT) {
+        un_send_fd(fdarray[i].fd, send_buf.fd, rank, send_buf.offset);
+        send_progress |= 1<<i;
+      }
+    }
+  }
+
+  for (int i = 0; i < world -1; ++i) {
+    // future_fds[i].wait();
+    // auto [fd, peer, offset] = future_fds[i].get();
+    auto [fd, peer, offset] = un_recv_fd(recv_socks[i]);
+    recv_buf[peer].fd = fd;
+    recv_buf[peer].offset = offset;
+  }
+
+  recv_buf[rank] = send_buf;
+}
 
 class timer 
 {
